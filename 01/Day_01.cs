2345455d109a6b6b6fdd5626586461543ea6346a
phortheman <marcuspullo@gmail.com>
2023-07-16 14:48:55 -0400

    Day 01
    Refactor

--- 01/Day_01.cs
+++ 01/Day_01.cs
@@ -1,62 +1,78 @@
-﻿using System.IO;
+﻿
+using System.IO;
 
-class Day_01
+namespace Day_01
 {
-    static void Main(string[] args)
+
+    public class Day_01_Solver
     {
-        List<int> input = GetInput();
+        public Day_01_Solver(){}
 
-        int partOne = GetNumberOfIncreases(input);
-        int partTwo = GetNumberOfIncreases(GetWindow(input));
-
-        Console.WriteLine($"The number of increases with the input is {partOne}");
-        Console.WriteLine($"The number of increases with the window is {partTwo}");
-    }
-
-    static List<int> GetInput()
-    {
-        List<int> input = new List<int>();
-
-        StreamReader reader = new StreamReader("01/input.txt");
-        String line = reader.ReadLine();
-
-        while (line != null)
+        public List<int> GenerateInput(String filePath)
         {
-            input.Add(int.Parse(line));
-            line = reader.ReadLine();
-        }
+            List<int> output = new List<int>();
 
-        reader.Close();
-
-        return input;
-    }
-
-    static int GetNumberOfIncreases(List<int> input, int lastIndex = 0)
-    {
-        if (lastIndex == 0)
-        {
-            lastIndex = input.Count;
-        }
-        int numOfIncreases = 0;
-        for (int i = 1; i < lastIndex; i++)
-        {
-            if (input[i - 1] < input[i])
+            using (StreamReader reader = new StreamReader(filePath))
             {
-                numOfIncreases++;
+                String? line;
+                while ( (line = reader.ReadLine() ) != null)
+                {
+                    output.Add(int.Parse(line));
+                }
             }
+
+            return output;
         }
-        return numOfIncreases;
-    }
 
-    static List<int> GetWindow(List<int> input)
-    {
-        List<int> window = new List<int>();
-
-        for (int i = 0; i < input.Count - 2; i++)
+        public int GetNumberOfIncreases(List<int> input, int lastIndex = 0)
         {
-            window.Add(input[i] + input[i + 1] + input[i + 2]);
+            if (lastIndex == 0)
+            {
+                lastIndex = input.Count;
+            }
+            int numOfIncreases = 0;
+            for (int i = 1; i < lastIndex; i++)
+            {
+                if (input[i - 1] < input[i])
+                {
+                    numOfIncreases++;
+                }
+            }
+            return numOfIncreases;
         }
 
-        return window;
+        public List<int> GetWindow(List<int> input)
+        {
+            List<int> window = new List<int>();
+
+            for (int i = 0; i < input.Count - 2; i++)
+            {
+                window.Add(input[i] + input[i + 1] + input[i + 2]);
+            }
+
+            return window;
+        }
+        static void Main(string[] args)
+        {
+            if( args.Length == 0)
+            {
+                Console.WriteLine("Please provide the puzzle input as a file");
+                return;
+            }
+
+            List<int> inputData;
+
+            Day_01_Solver solver = new Day_01_Solver();
+
+            inputData = solver.GenerateInput(args[0]);
+
+            int partOne = solver.GetNumberOfIncreases(inputData);
+            int partTwo = solver.GetNumberOfIncreases(solver.GetWindow(inputData));
+
+            Console.WriteLine($"The number of increases with the input is {partOne}");
+            Console.WriteLine($"The number of increases with the window is {partTwo}");
+        }
+
     }
+
 }
